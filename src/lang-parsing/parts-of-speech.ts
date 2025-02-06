cd8ce02a14cc4956321cff0c411fac28cc3e96dd
Frank M. Taylor <frtaylor@redhat.com>
2025-02-06 12:57:22 -0600

    improves pronoun detection w/ parsing and better regexing

---
+++ src/lang-parsing/parts-of-speech.ts
@@ -0,0 +1,30 @@
+const articleAndDemonstrativeRegex = /\b(a(n(y|other))?|some|th(e(se|ir)?|at|is|ose))\b/gi;
+const possessiveRegex = /\b(my|your|his|her|its|our|their)\b/gi;
+const prepositionRegex = /\b(?:ab(out|ove)|across|after|against|along|among|around|a(s|t)|be(fore|hind|low|neath|side|tween|yond)|but|by|con(sidering|cerning)|despite|down|during|except|excluding|for|from|in(side|to)?|like|near|o(f+)|on(to)?|opposite|out(side)?|over|past|per|regarding|since|through(out)?|till|to(ward)?|under|underneath|unlike|until|up|upon|via|with(in|out)?)\b/gi
+const adjectiveRegex = /\b(?:small|big|large|li(l|ttle)|premium|s(a|o)me)|live|white|black|brown|nice|best|worst|good|bad|fat|stupid|dumb|chubby\b/gi;
+const helperVerbRegex = /\b(?:let'?s|gonna|go|will|have|can)\b/gi;
+const personalPronounRegex = /\b(i|you|s?he|it|they|we)\b/gi;
+
+export function hasArticleOrDemonstrative(text: string): boolean {
+    return articleAndDemonstrativeRegex.test(text);
+}
+
+export function hasAdjective(text: string): boolean {
+    return adjectiveRegex.test(text);
+}
+
+export function hasPossessive(text: string): boolean {
+    return possessiveRegex.test(text);
+}
+
+export function hasHelperVerb(text: string): boolean {
+    return helperVerbRegex.test(text);
+}
+
+export function hasPreposition(text: string): boolean {
+    return prepositionRegex.test(text);
+}
+
+export function hasPersonalPronoun(text: string): boolean {
+    return personalPronounRegex.test(text);
+}
--- src/lang-parsing/pronouns.ts
+++ src/lang-parsing/pronouns.ts
@@ -1,60 +1,66 @@
-    /* regex: 
+import {Methodius} from 'methodius';
+import {hasArticleOrDemonstrative, hasPossessive, hasAdjective, hasPreposition, hasHelperVerb, hasPersonalPronoun} from './parts-of-speech';
+/* regex: 
       first part are the words that precede the word that would clue us that it's not a pronoun:
         we're looking for common modifiers:
         a, any, another, some, the, these,their, that, this, those
       second part is the pronoun itself: 
         chat, dude, bro, bruh, guy, sis, fam
     */
-const neoPronounRegex = /(?<!\b(a(n(y|other))?|some|th(e(se|ir)?|at|is|ose))\b\s)\b(chat|d(u+)de|br(o|uh)|guy|sis|fam)\b\s/gi
+const possiblePronounRegex = /(?<!\b(a(n(y|other))?|some|th(e(se|ir)?|at|is|ose))\b\s)\b(chat|d(u+)de|br((o+)|uh)|guy|sis|fam)\b\s/gi
 
-/*
-    We want to separately identify when chat acts as a verb. 
-    First part are the words that precede it that clues us in that it's a verb:
-    We're looking at either auxiliary verbs or pronouns:
-    lets, let's, I, you, we, the, they
-    We also use in, on which suggests that chat is a noun
-    Second part is the word chat itself
-    we have to leave out "to" because there are cases where chat is a direct object
-*/
-
-const chatAsVerbRegex = /(?<!\b(let'?s|i|you|(w|th)ey?|gonna|in|on)\b\s?)\b\s?(chat)\b\s?/gi;
-const chatAsNoun = /(?<!\b(in|on|about|small|big|large)\b\s?)\b\s?(chat)\b\s?/gi;
-const guyAsNoun = /(?<!\b(small|big|large|li(l|ttle)'?)\b\s?)\b\s?(guy)\b\s?/gi;
-const sisAsNoun = /(?<!\b(her|our|their|his|my|li(l|ttle)'?)\b\s?)\b\s?(guy)\b\s?/gi;
-
-export function hasNeoPronounInText(text: string): boolean {
-    return neoPronounRegex.test(text);
-}
-
-export function hasChatAsVerb(text: string): boolean {
-    return chatAsVerbRegex.test(text);
-}
-
-export function hasChatAsNoun(text: string): boolean {
-    return chatAsNoun.test(text);
-}
-
-export function hasGuyAsNoun(text: string): boolean {
-    return guyAsNoun.test(text);
-}
-
-export function hasSisAsNoun(text: string): boolean {
-    return sisAsNoun.test(text);
+export function hasPossiblePronounInText(text: string): boolean {
+    return possiblePronounRegex.test(text);
 }
 
 export function hasPronounInText(text: string): boolean {
-    const isNeo = hasNeoPronounInText(text);
-    const hasNoChatAsVerb = !hasChatAsVerb(text);
-    const hasNoChatAsNoun = !hasChatAsNoun(text);
-    const hasNoGuyAsNoun = !hasGuyAsNoun(text);
-    const hasNoSisAsNoun = !hasSisAsNoun(text);
+    const hasPossiblePronoun = hasPossiblePronounInText(text);
+    // if it doesn't have the pronoun, why keep going? 
+    if (!hasPossiblePronoun) {
+        return false;
+    }
+    // let's split into words
+    const wordBigrams = Methodius.getWordNGrams(text.toLowerCase());
+    // grab that word and make few assumptions
+    const targetPronoun = getPronounFromText(text);
+    // get bigrams that contain the pronoun
+    const bigramsWithPronoun = wordBigrams.filter((wordBigram) => wordBigram.includes(targetPronoun));
+    // if there's only one bigram with the pronoun, we're good. It's the start of the sentence.
+    // no need to check the rest
+    if (bigramsWithPronoun.length === 1) {
+        return true;
+    }
+    // there's multiple bigrams. Fine. We want the one with a word preceding the possible pronoun;
+    // that'll be the first one in this bigram array
+    const [bigramWithPronounAtEnd] = bigramsWithPronoun;
+    const [precedingWord] = bigramWithPronounAtEnd;
+    // now we need to check if that word is any part of speech that makes the "pronoun" a noun
+    const isArticleOrDemonstrative = hasArticleOrDemonstrative(precedingWord);
+    const isPossessive = hasPossessive(precedingWord);
+    const isAdjective = hasAdjective(precedingWord);
+    const isANoun = isArticleOrDemonstrative || isPossessive || isAdjective;
+    
+    // assume it's not a verb
+    let isAVerb = false;
+    // check if the word is chat, which can be a verb
+    const isTargetChat = targetPronoun.toLowerCase() === 'chat';
+    if (isTargetChat) {
+        const isPreposition = hasPreposition(precedingWord);
+        // is there a helper verb in front?
+        const isHelperVerb = hasHelperVerb(precedingWord);
+        // is there a personal pronoun in front?
+        const isPersonalPronoun = hasPersonalPronoun(precedingWord);
 
-    return isNeo && hasNoChatAsVerb && hasNoChatAsNoun && hasNoGuyAsNoun && hasNoSisAsNoun;
+        // if there's a personal pronoun or helper verb, chat is probably a verb
+        isAVerb = isHelperVerb || isPersonalPronoun || isPreposition;
+    }
+
+    return !isANoun && !isAVerb;
 }
 
 export function getPronounFromText(text: string = ''): string {
     if (!text) return '';
-    const match = text.match(neoPronounRegex);
+    const match = text.match(possiblePronounRegex);
     const pronoun = match
         ? match[0]
             ?.toLowerCase()
