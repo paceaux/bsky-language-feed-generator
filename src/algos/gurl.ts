8df77bf93122bc2226ab88e194ebdf44f6793ab2
Paceaux <frank.m.taylor08@gmail.com>
2026-05-27 22:32:33 -0500

    corrects mistake with gurl

--- src/algos/gurl.ts
+++ src/algos/gurl.ts
@@ -2,4 +2,4 @@ import {getPronounHandler} from '../algos/handlerFactory'
 // max 15 chars
 export const shortname = 'gurl'
 
-export const handler = getPronounHandler('boi')
+export const handler = getPronounHandler('gurl')
