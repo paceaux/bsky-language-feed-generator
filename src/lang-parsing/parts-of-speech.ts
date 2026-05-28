063f19a89d380ea91813f08637530c8b369d14c4
Paceaux <frank.m.taylor08@gmail.com>
2026-05-27 21:11:08 -0500

    ignores capture groups and adds y'all

--- src/lang-parsing/parts-of-speech.ts
+++ src/lang-parsing/parts-of-speech.ts
@@ -1,9 +1,9 @@
-const articleAndDemonstrativeRegex = /\b(a(n(y|other))?|some|th(e(se|ir)?|at|is|ose))\b/gi;
+const articleAndDemonstrativeRegex = /\b(a(?:n(?:y|other))?|some|th(?:e(?:se|ir)?|at|is|ose))\b/gi;
 const possessiveRegex = /\b(my|your|his|her|its|our|their)\b/gi;
-const prepositionRegex = /\b(?:ab(out|ove)|across|after|against|along|among|around|a(s|t)|be(fore|hind|low|neath|side|tween|yond)|but|by|con(sidering|cerning)|despite|down|during|except|excluding|for|from|in(side|to)?|like|near|o(f+)|on(to)?|opposite|out(side)?|over|past|per|regarding|since|through(out)?|till|to(wards?)?|under(neath)?|unlike|until|up(on)?|via|with(in|out)?)\b/gi
-const adjectiveRegex = /\b(?:small|big|large|li(l|ttle)|premium|s(a|o)me)|live|white|black|brown|nice|best|worst|good|bad|fat|stupid|dumb|chubby|random|weird|strange|quick|friendly|twitch|first|second|third|old|young\b/gi;
+const prepositionRegex = /\b(ab(?:out|ove)|across|after|against|along|among|around|a(?:s|t)|be(?:fore|hind|low|neath|side|tween|yond)|but|by|con(?:sidering|cerning)|despite|down|during|except|excluding|for|from|in(?:side|to)?|like|near|o(?:f+)|on(?:to)?|opposite|out(?:side)?|over|past|per|regarding|since|through(?:out)?|till|to(?:wards?)?|under(?:neath)?|unlike|until|up(?:on)?|via|with(?:in|out)?)\b/gi
+const adjectiveRegex = /\b(?:small|big|large|li(?:l|ttle)|premium|s(?:a|o)me)|live|white|black|brown|nice|best|worst|good|bad|fat|stupid|dumb|chubby|random|weird|strange|quick|friendly|twitch|first|second|third|old|young\b/gi;
 const helperVerbRegex = /\b(?:let'?s|gonna|go|will|have|can|wanna)\b/gi;
-const personalPronounRegex = /\b(i|you|s?he|it|they|we)\b/gi;
+const personalPronounRegex = /\b(?:i|you|s?he|it|they|we|y'all)\b/gi;
 
 export function hasArticleOrDemonstrative(text: string): boolean {
     return articleAndDemonstrativeRegex.test(text);
